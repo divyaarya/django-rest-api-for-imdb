514678e8a65d6f233949fe350193f63381dc2565
divyaarya <arya.divya01@gmail.com>
2016-10-06 23:29:09 +0530

    Update views.py

--- imdb/movies/views.py
+++ imdb/movies/views.py
@@ -70,4 +70,4 @@ def movie_edit(request, pk):
             return Response(status=status.HTTP_204_NO_CONTENT)
     elif request.method == 'DELETE':
         movie.delete()
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_204_NO_CONTENT)
